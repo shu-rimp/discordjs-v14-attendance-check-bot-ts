b4141e4467875074cb428b8dae6f40615df0e8a0
Sooji Park <soooojip@gmail.com>
2023-03-07 00:29:22 +0900

    #2 edit embed
    - edit embed when user clicks the button
    - update embed style

--- src/functions/embeds.ts
+++ src/functions/embeds.ts
@@ -1,12 +1,43 @@
 import { ColorResolvable, EmbedBuilder } from 'discord.js'
 import { EmbedConfig } from '../objects'
+import { Users } from '../types'
 
 export const createEmbed = () => {
     return new EmbedBuilder()
         .setColor(EmbedConfig.color as ColorResolvable)
-        .setTitle('타이틀')
-        .addFields({
-            name: '제목',
-            value: '내용',
-        })
+        .setTitle(EmbedConfig.title)
+        .setDescription(EmbedConfig.description)
+        .addFields(
+            { name: '이름', value: ' ', inline: true },
+            { name: '출석시간', value: ' ', inline: true }
+        )
+}
+
+export const createTopUserEmbed = () => {
+    return new EmbedBuilder()
+        .setColor(EmbedConfig.color as ColorResolvable)
+        .setTitle(EmbedConfig.title)
+        .setDescription(EmbedConfig.description)
+        .addFields(
+            { name: '이름', value: ' ', inline: true },
+            { name: '출석시간', value: ' ', inline: true }
+        )
+}
+
+export const editEmbed = (userValues: Users[]) => {
+    const userNames = userValues
+        .map(user => { return `${user.name}#${user.tag}` })
+        .join('\n')
+    const attendDates = userValues
+        .map(user => { return user.selectedAt })
+        .join('\n')
+
+    return new EmbedBuilder()
+        .setColor(EmbedConfig.color as ColorResolvable)
+        .setTitle(EmbedConfig.title)
+        .setDescription(EmbedConfig.description)
+        .addFields(
+            { name: '이름', value: userNames, inline: true },
+            { name: '출석시간', value: attendDates, inline: true }
+        )
 }

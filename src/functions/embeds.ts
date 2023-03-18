7be9f4898e7e9ee1754726bdc46fba946f7a101b
Sooji Park <soooojip@gmail.com>
2023-03-19 01:42:50 +0900

    #11 edit styles

--- src/functions/embeds.ts
+++ src/functions/embeds.ts
@@ -1,45 +1,65 @@
 import { ColorResolvable, EmbedBuilder } from 'discord.js'
 import { EmbedConfig } from '../objects'
-import { Users } from '../types'
+import { User, UserRow } from '../types'
 
 export const createEmbed = () => {
     return new EmbedBuilder()
         .setColor(EmbedConfig.color as ColorResolvable)
         .setTitle(EmbedConfig.title)
         .setDescription(EmbedConfig.description)
+        .setThumbnail(EmbedConfig.thumbnail)
         .addFields(
-            { name: '------------------------------------------------', value: ' '},
-            { name: '이름', value: ' ', inline: true },
-            { name: '출석시간', value: ' ', inline: true }
+            { name: '\u200B', value: ' ' }
         )
+        .setFooter({ text: EmbedConfig.footer })
 }
 
-export const createTopUserEmbed = () => {
+export const createTopUserEmbed = (foundUsers: UserRow[]) => {
+    const userNames = foundUsers
+        .map(user => user.user_display_name)
+        .join('\n')
+    const userTags = foundUsers
+        .map(user => `${user.user_name}#${user.user_tag}`)
+        .join('\n')
+    const attendCounts = foundUsers
+        .map(user => `${user.attendance_count}일`)
+        .join('\n')
+
     return new EmbedBuilder()
-        .setColor(EmbedConfig.color as ColorResolvable)
-        .setTitle(EmbedConfig.title)
-        .setDescription(EmbedConfig.description)
+        .setColor(EmbedConfig.colorOverview as ColorResolvable)
+        .setTitle(EmbedConfig.titleOverview)
+        .setDescription(EmbedConfig.descriptionOverview)
+        .setThumbnail(EmbedConfig.thumbnailOverview)
         .addFields(
-            { name: '이름', value: ' ', inline: true },
-            { name: '출석시간', value: ' ', inline: true }
+            { name: '\u200B', value: ' ' },
+            { name: '이름', value: `${userNames} `, inline: true },
+            { name: '태그', value: `${userTags} `, inline: true },
+            { name: '출석일', value: `${attendCounts} `, inline: true }
         )
 }
 
-export const editEmbed = (userValues: Users[]) => {
+export const editEmbed = (userValues: User[]) => {
     const userNames = userValues
-        .map(user => { return `${user.name}#${user.tag}` })
+        .map(user => user.displayName)
+        .join('\n')
+    const userTags = userValues
+        .map(user => `${user.name}#${user.tag}`)
         .join('\n')
     const attendDates = userValues
-        .map(user => { return user.selectedAt })
+        .map(user => user.selectedAt)
         .join('\n')
 
     return new EmbedBuilder()
         .setColor(EmbedConfig.color as ColorResolvable)
         .setTitle(EmbedConfig.title)
         .setDescription(EmbedConfig.description)
+        .setThumbnail(EmbedConfig.thumbnail)
         .addFields(
-            { name: '------------------------------------------------', value: ' '},
+            { name: '\u200B', value: ' ' },
             { name: '이름', value: userNames, inline: true },
-            { name: '출석시간', value: attendDates, inline: true }
+            { name: '태그', value: userTags, inline: true },
+            { name: '출석시간', value: attendDates, inline: true },
+            { name: '\u200B', value: ' ' }
         )
+        .setFooter({ text: EmbedConfig.footer })
 }
